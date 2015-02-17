15cdca18bcd122b4b5a9365d21b532ddb7f437dc
Andrew James <andrewpjames87@gmail.com>
2015-02-17 13:45:49 -0700

    User does not see clubs they dont belong to

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -1,13 +1,13 @@
 class ClubsController < ApplicationController
   before_action :authenticate
   def index
-    @clubs = Club.all
+    @clubs = current_student.clubs
   end
 
   def show
     @club = Club.find(params[:id])
     if not current_student.clubs.include?(@club)
-      render 'public/403.html'
+      render 'public/403.html', status: 403
     end
   end
 
